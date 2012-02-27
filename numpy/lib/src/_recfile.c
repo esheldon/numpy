296bf1a34ce0977f49fbabed4cea2dd194b060fc
Erin Sheldon <erin.sheldon@gmail.com>
2012-02-27 12:55:59 -0500

    binary slicing in place

--- numpy/lib/src/_recfile.c
+++ numpy/lib/src/_recfile.c
@@ -17,9 +17,7 @@ struct PyRecfileObject {
     PyObject* file_obj;
     FILE* fptr;
 
-    // the delimiter, NULL for binary
-    const char* delim;
-
+    int is_ascii;
     // One element for each column of the file, in order.
     // must be contiguous npy_intp arrays
     //PyObject* typecodes_obj;
@@ -31,6 +29,9 @@ struct PyRecfileObject {
     // jost pointers to the data sections
     npy_intp* sizes;
     npy_intp* nel;
+
+    // rowsize in bytes for binary
+    npy_intp rowsize;
 
     npy_intp nrows; // rows from the input offset of file
     npy_intp nfields;
@@ -63,7 +64,7 @@ static int _set_fptr(struct PyRecfileObject* self)
     return 1;
 }
 
-static npy_intp _get_nrows(PyObject* nrows_obj) {
+static npy_intp get_nrows(PyObject* nrows_obj) {
     npy_intp nrows=0;
     if (nrows_obj == Py_None) {
         // this shouldn't happen
@@ -78,25 +79,34 @@ static npy_intp _get_nrows(PyObject* nrows_obj) {
     return nrows;
 }
 
+static void set_rowsize(struct PyRecfileObject* self) {
+    npy_intp i=0;
+
+    self->rowsize=0;
+    for (i=0; i<self->nfields; i++) {
+        self->rowsize += self->sizes[i];
+    }
+}
+
 static int
 PyRecfileObject_init(struct PyRecfileObject* self, PyObject *args, PyObject *kwds)
 {
     PyObject* nrows_obj=NULL; // if sent, 1 element npy_intp array
     self->file_obj=NULL;
-    //self->typecodes_obj=NULL;
-    //self->typecodes=NULL;
+    self->is_ascii=0;
     self->scan_formats_obj=NULL;
     self->print_formats_obj=NULL;
     self->sizes_obj=NULL;
     self->sizes=NULL;
+    self->rowsize=0;
     self->nel_obj=NULL;
     self->nel=NULL;
     self->nrows=0;
     self->nfields=0;
     if (!PyArg_ParseTuple(args, 
-                          (char*)"OsOOOOO", 
+                          (char*)"OiOOOOO", 
                           &self->file_obj, 
-                          &self->delim,
+                          &self->is_ascii,
                           &self->sizes_obj,
                           &self->nel_obj,
                           &self->scan_formats_obj,
@@ -108,29 +118,129 @@ PyRecfileObject_init(struct PyRecfileObject* self, PyObject *args, PyObject *kwd
     if (!_set_fptr(self)) {
         return -1;
     }
-    self->nrows = _get_nrows(nrows_obj);
-    self->nfields = PyArray_SIZE((PyArrayObject*)self->sizes_obj);
-
+    self->nrows = get_nrows(nrows_obj);
     self->test=7;
     Py_XINCREF(self->sizes_obj);
     Py_XINCREF(self->nel_obj);
     Py_XINCREF(self->scan_formats_obj);
     Py_XINCREF(self->print_formats_obj);
 
+    self->nfields = PyArray_SIZE((PyArrayObject*)self->sizes_obj);
     self->sizes=(npy_intp*) PyArray_DATA((PyArrayObject*)self->sizes_obj);
     self->nel=(npy_intp*) PyArray_DATA((PyArrayObject*)self->nel_obj);
+
+    set_rowsize(self);
     return 0;
 }
 
 static PyObject *
 PyRecfileObject_repr(struct PyRecfileObject* self) {
-    return PyString_FromFormat("test: %d\nnrows: %ld\ndelim: '%s'\n", 
-            self->test, self->nrows, self->delim);
+    return PyString_FromFormat("test: %d\nnrows: %ld\n", 
+            self->test, self->nrows);
 }
 
 static PyObject *
 PyRecfileObject_test(struct PyRecfileObject* self) {
     return PyInt_FromLong((long)self->test);
+}
+
+
+int skip_binary_bytes(FILE* fptr, npy_intp nbytes) {
+    if (nbytes > 0) {
+        return fseeko(fptr, (off_t) nbytes, SEEK_CUR); 
+    }
+    return 0;
+}
+
+static PyObject* 
+read_binary_slice(struct PyRecfileObject* self,
+                  void* ptr, npy_intp start, npy_intp stop, npy_intp step) {
+
+    npy_intp current_row=0, row=0;
+    npy_intp bytes=0;
+
+    if (start > 0) {
+        if (0 != skip_binary_bytes(self->fptr, start*self->rowsize) ) {
+            // TODO use err string
+            PyErr_SetString(PyExc_IOError, "failed to seek");
+            return NULL;
+        }
+    }
+
+    current_row=start;
+    row=start;
+    while (row < stop) {
+        if (row > current_row) {
+            bytes = (row-current_row)*self->rowsize;
+            if (0 != skip_binary_bytes(self->fptr, bytes)) {
+                // TODO use err string
+                PyErr_SetString(PyExc_IOError, "failed to seek");
+                return NULL;
+            }
+            current_row=row;
+        }
+
+        //fprintf(stderr,"reading row: %ld\n", row);
+        if (1 != fread(ptr, (size_t)self->rowsize, 1, self->fptr)) {
+            // TODO use err string
+            PyErr_Format(PyExc_IOError, "failed to read row %ld", row);
+            return NULL;
+        }
+        //fprintf(stderr,"success\n");
+
+        ptr += self->rowsize;
+        current_row += 1;
+        row += step;
+    }
+    Py_RETURN_NONE;
+
+}
+
+/* 
+ * read row slice into input array.  As usual, error checking must be done in
+ * the python wrapper!  The exception is matching the itemsize of the
+ * input array to the rowsize in order to avoid out of bounds errors.
+ *
+ * we expect slices with step != 1 to be handled in the python wrapper.
+ */
+
+static PyObject* 
+PyRecfileObject_read_slice(struct PyRecfileObject* self, PyObject* args)
+{
+    PyObject* array=NULL;
+    PyObject* start_obj=NULL;
+    PyObject* stop_obj=NULL;
+    PyObject* step_obj=NULL;
+    npy_intp start=0, stop=0, step=0;
+    npy_intp itemsize=0;
+    void* ptr=NULL;
+    if (!PyArg_ParseTuple(args, (char*)"OOOO", 
+                          &array,
+                          &start_obj,
+                          &stop_obj,
+                          &step_obj)) {
+        return NULL;
+    }
+
+    start = *(npy_intp*) PyArray_DATA((PyArrayObject*)start_obj);
+    stop = *(npy_intp*) PyArray_DATA((PyArrayObject*)stop_obj);
+    step = *(npy_intp*) PyArray_DATA((PyArrayObject*)step_obj);
+
+    itemsize = PyArray_ITEMSIZE((PyArrayObject*) array);
+    if (itemsize != self->rowsize) {
+        PyErr_Format(PyExc_IOError, 
+                "input itemsize (%ld) != rowsize (%ld)",
+                itemsize, self->rowsize);
+        return NULL;
+    }
+
+    ptr = PyArray_DATA((PyArrayObject*) array);
+    if (self->is_ascii) {
+        fprintf(stderr,"implement ascii slicing\n");
+        return NULL;
+    } else {
+        return read_binary_slice(self, ptr, start, stop, step);
+    }
 }
 
 static void
@@ -168,7 +278,9 @@ cleanup(struct PyRecfileObject* self)
 static void
 PyRecfileObject_dealloc(struct PyRecfileObject* self)
 {
+
     cleanup(self);
+
 #if ((PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION >= 6) || (PY_MAJOR_VERSION == 3))
     Py_TYPE(self)->tp_free((PyObject*)self);
 #else
@@ -236,6 +348,7 @@ PyRecfile_get_formats(PyObject* self, PyObject *args) {
 
 static PyMethodDef PyRecfileObject_methods[] = {
     {"test",             (PyCFunction)PyRecfileObject_test,             METH_VARARGS,  "test\n\nReturn test value."},
+    {"read_slice",             (PyCFunction)PyRecfileObject_read_slice,             METH_VARARGS,  "read a slice into input array. Use for step != 1, otherwise can use numpy.fromfile\n"},
     {NULL}  /* Sentinel */
 };
 
